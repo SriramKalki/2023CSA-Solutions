adb648ec544b46a61eacd950abc12758dbe78775
SriramKalki <79238833+SriramKalki@users.noreply.github.com>
2023-05-06 12:06:29 -0500

    Update 4b.java

--- 4b.java
+++ 4b.java
@@ -1,5 +1,5 @@
 public Candy removeNextByFlavor(String flavor){
-    for(int i = box.length - 1; i >= 0; i++){
+    for(int i = box.length - 1; i >= 0; i--){
         for(int j = 0; j < box[i].length; j++){
             //box[i][j]
             if(box[i][j] == null) continue;
