b7fd86f9a9039137900daaab7ac106fe3d38a0b1
SriramKalki <79238833+SriramKalki@users.noreply.github.com>
2023-05-09 11:59:24 -0500

    Update 1a.java

--- 1a.java
+++ 1a.java
@@ -2,7 +2,7 @@ public int findFreeBlock(int period, int duration){
     for(int i = 0; i <= 60 - duration; i++){
         boolean valid = true;
         for(int j = i; j < i + duration; j++){
-            if(!isMinuteFree(period,duration)){
+            if(!isMinuteFree(period,i)){
                 valid = false;
                 break;
             }
